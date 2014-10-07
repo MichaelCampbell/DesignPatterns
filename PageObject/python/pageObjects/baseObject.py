abd22651837b3b253116c9a69e82ff19fbef6bf7
Russell Bradley <me@russellbradley.com>
2014-10-07 00:03:35 -0400

    - add enter_text, and click_button method

--- PageObject/python/pageObjects/baseObject.py
+++ PageObject/python/pageObjects/baseObject.py
@@ -1,8 +1,14 @@
 class Base(object):
     
-    def _init_(self, wd):
-        self.wd = wd
+    def _init_(self, driver):
+        self.driver = driver
     
     def find(self, locator):
-        return self.wd.find_element_by_xpath(self.current_locator(locator)
+        return self.driver.find_element_by_xpath(self.current_locator(locator)
+
+    def enter_text(self, locator, text):
+        self.find(locator).send_keys(text)
+
+    def click_button(self, locator):
+        self.find(locator).click()
 
