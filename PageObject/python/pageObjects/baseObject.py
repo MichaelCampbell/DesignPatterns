89cb3b5c160959a5dfd95c12d09a157f49bd88aa
Russell Bradley <me@russellbradley.com>
2014-10-07 00:08:32 -0400

    - fix syntax error

--- PageObject/python/pageObjects/baseObject.py
+++ PageObject/python/pageObjects/baseObject.py
@@ -4,7 +4,7 @@ class Base(object):
         self.driver = driver
     
     def find(self, locator):
-        return self.driver.find_element_by_xpath(self.current_locator(locator)
+        return self.driver.find_element_by_xpath(self.current_locator(locator))
 
     def enter_text(self, locator, text):
         self.find(locator).send_keys(text)
