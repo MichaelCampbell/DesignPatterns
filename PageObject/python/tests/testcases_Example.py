7991060714e6a7ad80de8791408da4ce7479d1ce
Russell Bradley <me@russellbradley.com>
2014-10-08 23:29:01 -0400

    - add testcase example

---
+++ PageObject/python/tests/testcases_Example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from testcases_Base import TestCaseBase
+
+class TestCaseExample(TestCaseBase):
+    
+    def test_1_example(self):
+        pass
+
+    def test_2_example(self):
+        pass
+
+# etc... additional test cases as need.
+
