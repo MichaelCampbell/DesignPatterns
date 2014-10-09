4bae4ed00c42188cc49ad3519b954445315da932
Russell Bradley <me@russellbradley.com>
2014-10-08 23:28:31 -0400

    - add testcase base

---
+++ PageObject/python/tests/testcases_Base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from pageObjects.baseObject import Base
+
+from selenium import webdriver
+
+from unittest import TestCase
+
+class TestCaseBase(TestCase):
+
+    def setUp(self):
+        pass 
+
+    def tearDown(self):
+        pass
+
