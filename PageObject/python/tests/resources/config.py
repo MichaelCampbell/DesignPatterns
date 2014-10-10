d4b5090bd974df64e1a0df5bc47e379ffa09f4e7
Russell Bradley <me@russellbradley.com>
2014-10-09 21:29:22 -0400

    - removed unused args argument

--- PageObject/python/tests/resources/config.py
+++ PageObject/python/tests/resources/config.py
@@ -2,7 +2,7 @@
 
 class Config(object):
     
-    def config(self, args):
+    def config(self):
         desired_caps   = {}
         desired_caps['version'] = '1.0'
         desired_caps['browser'] = 'netscape'
