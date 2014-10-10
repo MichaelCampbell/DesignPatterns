1006c54c2d5a50e4cfe504ff1578893d621274d7
Russell Bradley <me@russellbradley.com>
2014-10-09 21:52:45 -0400

    - add example pageObjectA

---
+++ PageObject/python/tests/pageObjects/pageObjectA.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from baseObject import Base
+
+class PageA(Base):
+    
+    def __init__(self, driver):
+        super(PageA, self).__init__(driver)
+        self.foo = foo
+        self.bar = bar
+
