0100988bad0656a0a295fdcb8f5bca37f13f5f58
Russell Bradley <me@russellbradley.com>
2014-10-10 13:59:37 -0400

    - import pageObjectA into base - assign PageA

--- PageObject/python/tests/testcases_Base.py
+++ PageObject/python/tests/testcases_Base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from pageObjects.baseObject import Base
+from pageObjects.pageObjectA import PageA 
 from resources.config import Config
 
 from selenium import webdriver
@@ -12,6 +12,7 @@ class TestCaseBase(TestCase):
     def setUp(self):
         self.setup = Config()
         self.driver = webdriver.Remote('http://0.0.0.0:4723/wd/hub', self.setup.config())
+        self.pageA = PageA(self.driver)
         pass
 
     def tearDown(self):
