c9179257316d3b8e546e8d32d312a53f3e738499
Russell Bradley <me@russellbradley.com>
2014-10-11 17:49:55 -0400

    - find locator method

--- PageObject/python/tests/pageObjects/baseObject.py
+++ PageObject/python/tests/pageObjects/baseObject.py
@@ -7,12 +7,15 @@ class Base(object):
     def _init_(self, driver):
         self.driver = driver
     
+    def current_locator(self, locator):
+        return locators[locator]
+
     def find(self, locator):
         return self.driver.find_element_by_xpath(self.current_locator(locator))
 
     def enter_text(self, locator, text):
-        self.find(locator).send_keys(text)
+        self.find(self.current_locator(locator).send_keys(text))
 
     def click_button(self, locator):
-        self.find(locator).click()
+        self.find(self.current_locator(locator)).click()
 
