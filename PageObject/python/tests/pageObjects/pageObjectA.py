0cda0b5d2a7327b3f5fd0485a58287b4e3620a91
Russell Bradley <me@russellbradley.com>
2014-10-11 18:06:48 -0400

    - add def examples - include foo, bar, in constructor

--- PageObject/python/tests/pageObjects/pageObjectA.py
+++ PageObject/python/tests/pageObjects/pageObjectA.py
@@ -4,8 +4,18 @@ from baseObject import Base
 
 class PageA(Base):
     
-    def __init__(self, driver):
+    def __init__(self, driver, foo, bar):
         super(PageA, self).__init__(driver)
         self.foo = foo
         self.bar = bar
+
+    def enter_text_and_submit(self):
+        self.enter_test_on_pageA()
+        self.submit_button_on_PageA()
+
+    def enter_text_on_pageA(self,locator):
+        self.enter_text("PAGE_A_TEXTFIELD")
+
+    def submit_button_on_pageA(self, locator):
+        self.click_button("PAGE_A_BUTTON")
 
