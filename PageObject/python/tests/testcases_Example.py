e77905e358af0feb1c4868de52f8ecf87be5ecfd
Russell Bradley <me@russellbradley.com>
2014-10-12 23:35:29 -0400

    - add testcase examples

--- PageObject/python/tests/testcases_Example.py
+++ PageObject/python/tests/testcases_Example.py
@@ -3,12 +3,15 @@
 from testcases_Base import TestCaseBase
 
 class TestCaseExample(TestCaseBase):
-    
+
+# Test examples using the page objects functions imported into TestCaseBas
+
     def test_1_example(self):
-        pass
+        self.pageA.enter_text_and_submit()
 
     def test_2_example(self):
-        pass
+        self.pageA.enter_text_on_pageA()
+        self.pageA.submit_button_on_pageA()
 
 # etc... additional test cases as need.
 
