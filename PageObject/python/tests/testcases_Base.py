387530cb66f52e72abc774eff587cbc78809a294
Russell Bradley <me@russellbradley.com>
2014-10-13 23:41:19 -0400

    - comments on setup and teardown methods

--- PageObject/python/tests/testcases_Base.py
+++ PageObject/python/tests/testcases_Base.py
@@ -9,12 +9,14 @@ from unittest import TestCase
 
 class TestCaseBase(TestCase):
 
+# The setup method whould kick off before everytest
     def setUp(self):
         self.setup = Config()
         self.driver = webdriver.Remote('http://0.0.0.0:4723/wd/hub', self.setup.config())
         self.pageA = PageA(self.driver)
         pass
 
+# The tear down method should kickoff everytime at the end of your method
     def tearDown(self):
         self.driver.quit()
         pass
