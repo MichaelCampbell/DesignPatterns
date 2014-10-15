accb64b0a2d49d5713adc92803ab042bc5573a56
Russell Bradley <me@russellbradley.com>
2014-10-15 00:15:31 -0400

    - page objectA commenting

--- PageObject/python/tests/pageObjects/pageObjectA.py
+++ PageObject/python/tests/pageObjects/pageObjectA.py
@@ -3,19 +3,23 @@
 from baseObject import Base
 
 class PageA(Base):
-    
+
     def __init__(self, driver, foo, bar):
         super(PageA, self).__init__(driver)
         self.foo = foo
         self.bar = bar
 
-    def enter_text_and_submit(self):
+# Function created from helper methods below
+
+def enter_text_and_submit(self):
         self.enter_test_on_pageA()
         self.submit_button_on_PageA()
 
+# Helper methods
+
     def enter_text_on_pageA(self,locator):
-        self.enter_text("PAGE_A_TEXTFIELD")
+        self.enter_text("PAGE_A_TEXTFIELD") # <- locators there are no css ids or xpaths in pageobjects
 
     def submit_button_on_pageA(self, locator):
-        self.click_button("PAGE_A_BUTTON")
+        self.click_button("PAGE_A_BUTTON") # <- locators - there are no css ids or xpaths in pageobjects. 
 
