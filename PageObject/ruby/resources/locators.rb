cebb4903f43fbb0ccd996b8d037ca647b1f7f9e7
Russell Bradley <me@russellbradley.com>
2014-10-30 17:46:34 -0400

    - remove print used for testing

--- PageObject/ruby/resources/locators.rb
+++ PageObject/ruby/resources/locators.rb
@@ -5,5 +5,3 @@ locator["page A text field"] = "xpath for text field A"
 locator["page B button"]     = "cssid for button B"
 locator["page B text field"] = "cssid for text field B"
 
-puts locator["page B text field"]
-
