da2125d4ccacf65f94c5628a9f527bf368bcd1f2
Russell Bradley <me@russellbradley.com>
2014-10-30 17:04:08 -0400

    - locator example changes to resemble python example"

--- PageObject/ruby/resources/locators.rb
+++ PageObject/ruby/resources/locators.rb
@@ -1,6 +1,8 @@
 locator = {
 
-    "button A" => "xpath for button a",
-    "submit button" => "css id for submit button"
+  "page A button" => "xpath for button a",
+  "page A text field" => "xpath for text field a",
+  "page B button" => "cssid for button B",
+  "page B text field" => "cssid for text field B",
 
 }
