4769d02a29e67312fd248a656f56a8cf67cf68e4
Russell Bradley <me@russellbradley.com>
2014-10-31 15:50:07 -0400

    - comments"

--- PageObject/ruby/resources/locators.rb
+++ PageObject/ruby/resources/locators.rb
@@ -1,5 +1,9 @@
+#!/usr/bin/env ruby
+
+# Creates a new Hash called locator
 locator = {}
 
+# assigns key value to hash
 locator["page A button"]     = "xpath for button A"
 locator["page A text field"] = "xpath for text field A"
 locator["page B button"]     = "cssid for button B"
