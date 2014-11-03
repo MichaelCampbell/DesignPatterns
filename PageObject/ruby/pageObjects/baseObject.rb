845f19486cb445ad6f7800b8fd47224a40950199
Russell Bradley <me@russellbradley.com>
2014-11-02 23:14:48 -0500

    - additional base methods

--- PageObject/ruby/pageObjects/baseObject.rb
+++ PageObject/ruby/pageObjects/baseObject.rb
@@ -6,4 +6,24 @@ class Base
     @driver = driver
   end
 
+  def visit(url='/')
+    driver.get(ENV['base_url'] + url)
+  end
+
+  def find(locator)
+    driver.find_element locator
+  end
+
+  def clear(locator)
+    find(locator).clear
+  end
+
+  def type(locator, input)
+    find(locator).send_keys input
+  end
+
+  def click_on(locator)
+    find(locator).click
+  end
+
 end
