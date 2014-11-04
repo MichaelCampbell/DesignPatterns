78e8d0977c127d81b045a3db0dbabc7f501eb9fa
Russell Bradley <me@russellbradley.com>
2014-11-03 23:07:04 -0500

    - additional page a example

--- PageObject/ruby/pageObjects/pageA.rb
+++ PageObject/ruby/pageObjects/pageA.rb
@@ -1,5 +1,6 @@
 # imports baseObject
 require './baseObject'
+require '../resources/locators'
 
 # inherits from base
 class PageA < Base
@@ -7,7 +8,19 @@ class PageA < Base
   def initialize(driver)
     super
     visit
-    verify_page
+  end
+
+  def send_text_to_pageA()
+    send_text_to_page(locator["page A button"], locator["page A text field"], 'example text a')
+  end 
+
+  def send_text_to_pageB()
+    send_text_to_page(locator["page B button"], locator["page A text field"], 'example text b')
+  end
+
+  def send_text_to_page(button, textfield, input)
+    click_on(button)
+    type(textfield, input)
   end
   
 end
