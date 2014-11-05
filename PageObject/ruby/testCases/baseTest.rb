708dcd414a2148b527563137121cea6c6020423c
Russell Bradley <me@russellbradley.com>
2014-11-04 23:50:57 -0500

    - comments

--- PageObject/ruby/testCases/baseTest.rb
+++ PageObject/ruby/testCases/baseTest.rb
@@ -1,15 +1,20 @@
+# imports selenium
 require 'selenium-webdriver'
+# imports pageObject examplePage
 require_relative '../pageObjects/examplePage'
 
+# occurs before every test
 def setup
   @driver = Selenium::WebDriver.for :firefox
   ENV['base_url'] = 'http://www.example.com'
 end
 
+# occurs at the end of every test
 def teardown
   @driver.quit
 end
 
+# calls the setup and teardown methods, then the run block below
 def run
   setup
   yield
