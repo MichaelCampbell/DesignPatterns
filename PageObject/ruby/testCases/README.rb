66ee071c14fe923d19949f3bd323040e34707a92
Russell Bradley <me@russellbradley.com>
2014-11-04 23:55:37 -0500

    - readme update

--- PageObject/ruby/testCases/README.rb
+++ PageObject/ruby/testCases/README.rb
@@ -1,1 +1,5 @@
-basetest readme
+The baseTest should contain all of your test logic.
+You will import the page objects into your test base class
+
+Each additional test should inherit from the test base class
+These could be ran from a yield, or additional file per test.
